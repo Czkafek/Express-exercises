1359847b22420ec2f543282d377b698862a9abb7
Czkafek <czkafek@gmail.com>
2025-03-01 11:17:57 +0100

    bcrypt learned

--- user/controllers/user.controller.js
+++ user/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/User.model.js");
+const bcrypt = require("bcrypt");
 
 const getUsers = async (req, res) => {
     try {
@@ -11,7 +12,10 @@ const getUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
     try {
-        const user = await User.create(req.body);
+        const { password, name } = req.body
+        const salt = await bcrypt.genSalt();
+        const newPassword = await bcrypt.hash(password, salt);
+        const user = await User.create({ name: name, password: newPassword });
         res.status(200).json({ message: "User has been successfully created" });
     } catch (err) {
         res.status(500).json({ message: err })
